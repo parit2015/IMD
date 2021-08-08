d8adbb7a515f6be05cedd4a1a69d4aba6b32abab
Paritosh "Parit <parit2015@users.noreply.github.com>
2021-08-08 19:01:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,37 +2,33 @@ package main
 
 import (
 	"Sugarbox/helper"
+	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
-var collectionMovies, collectionUsers, 
+var collectionMovies, collectionUsers,
 	collectionMappings = helper.GetCollection(helper.GetDBConnection("mongodb://localhost:27017"))
 
 func main() {
 	r := mux.NewRouter()
-	
+
 	/*
-	These endpoints are used to populate/access information to/from DB. 
-	As per requirement, we dont want this functionality to exposed from the Application.
-	Keeping it internal.
-	 */
+		These endpoints are used to populate/access information to/from DB.
+		As per requirement, we dont want this functionality to exposed from the Application.
+		Keeping it internal.
+	*/
 	r.HandleFunc("/IMDB/movies", createMovie).Methods("POST")
 	r.HandleFunc("/IMDB/users", createUsers).Methods("POST")
 	r.HandleFunc("/IMDB/mappings", createMappings).Methods("POST")
-	//r.HandleFunc("/IMDB/deleteCollections", deleteCollectionMovies).Methods("DELETE")
-	//r.HandleFunc("/IMDB/getMappings", getMappings).Methods("GET")
-	//r.HandleFunc("/IMDB/getUsers", getUsers).Methods("GET")
-	//r.HandleFunc("/IMDB/getMovies", getMovies).Methods("GET")
-	
+
 	/*
-	The set of endpoints to be exposed as northbound APIs
-	 */
+		The set of endpoints to be exposed as northbound APIs
+	*/
 	r.HandleFunc("/IMDB/movies/{movie-name}", getMovie).Methods("GET")
 	r.HandleFunc("/IMDB/moviesByUser/{user-id}", getMoviesByUser).Methods("GET")
 	r.HandleFunc("/IMDB/updateRating/{user-id}/{movie-name}", updateRating).Methods("PUT")
 	r.HandleFunc("/IMDB/addComment/{user-id}/{movie-name}", addComment).Methods("PUT")
-	
-	log.Fatal(http.ListenAndServe(":8000", r))
+
+	fmt.Println(http.ListenAndServe(":8000", r))
 }
