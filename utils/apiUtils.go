2c374894901012be706beb88ab0192e1d3da4e97
Paritosh "Parit <parit2015@users.noreply.github.com>
2021-08-09 13:10:13 +0530

    Update apiUtils.go

--- utils/apiUtils.go
+++ utils/apiUtils.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 import (
 	"IMD-master/models"
@@ -8,31 +8,16 @@ import (
 	"net/http"
 )
 
-func getOldNewDocument(request *http.Request, collection *mongo.Collection,
+func GetOldNewDocument(request *http.Request, collection *mongo.Collection,
 	DocOld *models.MovieUserMappingInformation, DocNew *models.MovieUserMappingInformation, filter bson.M) {
 
-	findOne(collection, filter, DocOld)
+	FindOne(collection, filter, DocOld)
 
 	_ = json.NewDecoder(request.Body).Decode(DocNew)
 }
 
-func updateDB(collection *mongo.Collection, filter bson.M, tobeUpdateBSONDoc bson.D,
+func UpdateDB(collection *mongo.Collection, filter bson.M, tobeUpdateBSONDoc bson.D,
 	targetObj *models.MovieUserMappingInformation) {
 
 	findOneAndUpdate(collection, filter, tobeUpdateBSONDoc, targetObj)
-}
-
-func createMappings(writer http.ResponseWriter, request *http.Request) {
-	var mappings []models.MovieUserMappingInformation
-	insertDocs(collectionMappings, writer, request, mappings)
-}
-
-func createUsers(writer http.ResponseWriter, request *http.Request) {
-	var users []models.UserInformation
-	insertDocs(collectionUsers, writer, request, users)
-}
-
-func createMovie(writer http.ResponseWriter, request *http.Request) {
-	var movies []models.MovieInformation
-	insertDocs(collectionMovies, writer, request, movies)
 }
