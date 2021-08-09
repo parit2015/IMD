02f4aaf0ac1ac299612bd516bffb85227d81c1f5
Paritosh "Parit <parit2015@users.noreply.github.com>
2021-08-09 11:36:27 +0530

    Update dbUtils.go

--- dbUtils.go
+++ dbUtils.go
@@ -9,14 +9,14 @@ import (
 
 func findMany(collection *mongo.Collection, filter bson.M) (*mongo.Cursor, error) {
 	/*
-		This function finds more than one document
+	This function finds more than one document
 
-		params:
-			collection: Collection on which the search has to be performed
-			filter: Filtering condition of the search
+	params:
+		collection: Collection on which the search has to be performed
+		filter: Filtering condition of the search
 
-		returns:
-			mongo-cursor: Collection of resultant document
+	returns:
+		mongo-cursor: Collection of resultant document
 	*/
 	var targetObj *mongo.Cursor
 	targetObj, err := collection.Find(context.TODO(), filter)
@@ -30,12 +30,12 @@ func findMany(collection *mongo.Collection, filter bson.M) (*mongo.Cursor, error
 
 func findOne(collection *mongo.Collection, filter bson.M, targetObj interface{}) {
 	/*
-		This function finds one and only one document
+	This function finds one and only one document
 
-		params:
-			collection: Collection on which the search has to be performed
-			filter: Filtering condition of the search
-			targetObj: Object on which the resultant document has to be saved
+	params:
+		collection: Collection on which the search has to be performed
+		filter: Filtering condition of the search
+		targetObj: Object on which the resultant document has to be saved
 	*/
 	err := collection.FindOne(context.TODO(), filter).Decode(targetObj)
 	if err != nil {
@@ -45,12 +45,12 @@ func findOne(collection *mongo.Collection, filter bson.M, targetObj interface{})
 
 func findOneAndUpdate(collection *mongo.Collection, filter bson.M, tobeUpdatedInfo bson.D, targetObj interface{}) {
 	/*
-		This function finds and update one document
+	This function finds and update one document
 
-		params:
-			collection: Collection on which the search has to be performed
-			filter: Filtering condition of the search
-			tobeUpdatedInfo: The payload, that needs to be updated in the filtered document
+	params:
+		collection: Collection on which the search has to be performed
+		filter: Filtering condition of the search
+		tobeUpdatedInfo: The payload, that needs to be updated in the filtered document
 	*/
 	err := collection.FindOneAndUpdate(context.TODO(), filter, tobeUpdatedInfo).Decode(targetObj)
 	if err != nil {
