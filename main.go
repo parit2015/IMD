7f128bfaf196aea38a05ca9fc35e35adad002981
Paritosh "Parit <parit2015@users.noreply.github.com>
2021-08-09 11:02:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ func main() {
 	r := mux.NewRouter()
 
 	/*
-		These endpoints are used to populate/access information to/from DB.
-		As per requirement, we dont want this functionality to exposed from the Application.
-		Keeping it internal.
+	These endpoints are used to populate/access information to/from DB.
+	As per requirement, we dont want this functionality to exposed from the Application.
+	Keeping it internal.
 	*/
 	r.HandleFunc("/IMDB/movies", createMovie).Methods("POST")
 	r.HandleFunc("/IMDB/users", createUsers).Methods("POST")
 	r.HandleFunc("/IMDB/mappings", createMappings).Methods("POST")
 
 	/*
-		The set of endpoints to be exposed as northbound APIs
+	The set of endpoints to be exposed as northbound APIs
 	*/
 	r.HandleFunc("/IMDB/movies/{movie-name}", getMovie).Methods("GET")
 	r.HandleFunc("/IMDB/moviesByUser/{user-id}", getMoviesByUser).Methods("GET")
